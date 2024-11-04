a3442facc65e8c61dccaddad653cfa73db289a47
cjee21 <77721854+cjee21@users.noreply.github.com>
2024-11-04 20:08:53 +0800

    Add docstrings

--- DFRobot_DHT20.py
+++ DFRobot_DHT20.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*
 
-# Originally from https://github.com/DFRobot/DFRobot_DHT20/tree/master/python/raspberrypi
-#
-# Modified by https://github.com/cjee21/
-# MIT License
-# Copyright (c) 2022-2023 cjee21
-# 
-#
-# Modifications:
-#
-# 25 February 2022
-# - use smbus2
-# - add decimal to temperature formula
-# - add get_temperature_and_humidity function for getting temperature and humidity in a single read
-#
-# 16 January 2023
-# - correct sensor initialization check and clean up init (begin) function
-# - improve get_temperature_and_humidity function
-# - add CRC-8 checking
-# - remove functions no longer needed
-# - add debug logging
-# Note: Sensor initialization and CRC-8 calculation is based on Aosong sample code from
-#       http://aosong.com/userfiles/files/software/AHT20-21%20DEMO%20V1_3(1).rar
-
 """
+### Raspberry Pi Python library for DHT20 Temperature and Humidity Sensor with CRC verification.
+### https://github.com/cjee21/RPi-DHT20
+
+Originally from https://github.com/DFRobot/DFRobot_DHT20/tree/master/python/raspberrypi
+
+| Modified by https://github.com/cjee21/
+| MIT License
+| Copyright (c) 2022-2023 cjee21
+
+Modifications:
+- 25 February 2022
+  - use `smbus2`
+  - add decimal to temperature formula
+  - add `get_temperature_and_humidity` function for getting temperature and humidity in a single read
+- 16 January 2023
+  - correct sensor initialization check and clean up init (begin) function
+  - improve `get_temperature_and_humidity` function
+  - add CRC-8 checking
+  - remove functions no longer needed
+  - add debug logging
+
+Note:
+  Sensor initialization and CRC-8 calculation is based on Aosong sample code from
+  http://aosong.com/userfiles/files/software/AHT20-21%20DEMO%20V1_3(1).rar
+
+Original:
   *@file DFRobot_DHT20.py
   *@brief Define the basic structure of class DFRobot_DHT20
   *@copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
@@ -50,41 +53,59 @@ class DFRobot_DHT20(object):
 
 
   def __init__(self, bus: int, address: int) -> None:
+    """Initialize the class.
+
+    Args:
+      bus: The I2C bus that DHT20 is connected to. For example:
+        0x01
+      address: The I2C address of DHT20. It is:
+        0x38
+    """
     self.i2cbus = smbus.SMBus(bus)
     self._addr = address
 
 
-  # Sensor initialization function
-  # @return Return True if initialization succeeds, otherwise return False.
   def begin(self) -> bool:
+    """Sensor initialization.
+
+    Returns:
+      True if initialization succeeds, otherwise False.
+    """
 
     # after power-on, wait no less than 100ms
     time.sleep(0.5)
 
     # check and return initialization status
-    data = self.read_reg(0x71,1)
+    data = self.__read_reg(0x71,1)
     if (data[0] & 0x18) != 0x18:
       return False
     else:
       return True
 
 
-  # Get both temperature and humidity in a single read
-  # @return Return temperature (C), humidity (%) and CRC result (True if error else False) as tuple
   def get_temperature_and_humidity(self) -> tuple[float, float, bool]:
+    """Get both temperature and humidity in a single read.
+
+    [!] Do not call this function more often than 2 seconds as recommended by the
+    datasheet to prevent rise in sensor temperature that will affect its accuracy. 
+
+    Returns:
+      The temperature (±0.5℃), humidity (±3%) and 
+      CRC result (True if error else False) as a tuple.
+    """
 
     # trigger measurement
-    self.write_reg(0xac,[0x33,0x00])
+    self.__write_reg(0xac,[0x33,0x00])
 
     # wait 80 ms and keep waiting until the measurement is completed
     while True:
       time.sleep(0.08)
-      data = self.read_reg(0x71,1)
+      data = self.__read_reg(0x71,1)
       if (data[0] & 0x80) == 0:
         break
 
     # read sensor data
-    data = self.read_reg(0x71,7)
+    data = self.__read_reg(0x71,7)
 
     # extract and convert temperature and humidity from data
     temperature_rawData: int = ((data[3]&0xf) << 16) + (data[4] << 8) + data[5]
@@ -93,13 +114,13 @@ class DFRobot_DHT20(object):
     humidity: float = float(humidity_rawData) / 0x100000 * 100
 
     # check CRC
-    crc_error: bool = self.calc_CRC8(data) != data[6]
+    crc_error: bool = self.__calc_CRC8(data) != data[6]
 
     # debug logging
     if logger.isEnabledFor(logging.DEBUG):
       logger.debug("Raw data      : 0x" + ''.join(format(x, '02x') for x in data))
       logger.debug("Read CRC      : " + hex(data[6]))
-      logger.debug("Calculated CRC: " + hex(self.calc_CRC8(data)))
+      logger.debug("Calculated CRC: " + hex(self.__calc_CRC8(data)))
       logger.debug("Temperature   : %f\u00b0C", temperature)
       logger.debug("Humidity      : %f %%", humidity)
 
@@ -107,10 +128,15 @@ class DFRobot_DHT20(object):
     return (temperature, humidity, crc_error)
 
 
-  # CRC function
-  # @param message - data from sensor which its CRC-8 is to be calculated
-  # @return Return calculated CRC-8
-  def calc_CRC8(self, data: list[int]) -> int:
+  def __calc_CRC8(self, data: list[int]) -> int:
+    """Calculate CRC-8.
+
+    Args:
+      data: Data from sensor which its CRC-8 is to be calculated.
+
+    Returns:
+      Calculated CRC-8
+    """
 
     crc: int = 0xFF
 
@@ -125,12 +151,12 @@ class DFRobot_DHT20(object):
     return (crc & 0xFF)
 
   
-  def write_reg(self, reg: int, data: list[int]) -> None:
+  def __write_reg(self, reg: int, data: list[int]) -> None:
     time.sleep(0.01)
     self.i2cbus.write_i2c_block_data(self._addr,reg,data)
   
   
-  def read_reg(self, reg: int, len: int) -> list[int]:
+  def __read_reg(self, reg: int, len: int) -> list[int]:
     time.sleep(0.01)
     rslt: list[int] = self.i2cbus.read_i2c_block_data(self._addr,reg,len)
     #print(rslt)
